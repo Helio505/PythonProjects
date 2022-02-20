e597a0f77386d9a39dbd94279c9b643ab51bd3cb
Helio <95880348+HelioHBR@users.noreply.github.com>
2022-02-19 21:02:52 -0300

    Update My first imput test.cpp

--- C++/My first imput test.cpp
+++ C++/My first imput test.cpp
@@ -1,1 +1,20 @@
+// I have no idea what these do
+#include <iostream>
+using namespace std;
 
+int main() 
+{    
+    //from what I understand this {number} is a variable
+    int number;
+
+    //cout I think it means something out, as in something going out, not an input.
+    cout << "Enter an integer: ";
+    //cin Seem to be input, so something going in.
+    // I think my input is being stored in the {number} variable
+    cin >> number;
+
+    // so here it is sort of doing print("You entered ", number)
+    cout << "You entered " << number;
+    //also no idea what this does
+    return 0;
+}
