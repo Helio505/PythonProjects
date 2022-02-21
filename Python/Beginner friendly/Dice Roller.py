21cf9fafb38c411bef2c87f73ba975d3578da71d
Helio <95880348+HelioHBR@users.noreply.github.com>
2022-02-21 00:44:11 -0300

    Dice Roller

---
+++ Python/Beginner friendly/Dice Roller.py
@@ -0,0 +1,11 @@
+import random
+
+# Dice roller
+# objective: you'll run the code and python will print a random number from 1 to 9
+
+# random_number is a variable, it can store things.
+# the _ in the middle is just a way of organizing things. When you have a lot of variables it is easier.
+# random.randint here you are using randint(), and you are able to do that because you imported the random library module.
+random_number = random.randint(1, 9) # here you are saying randint(start here, end here), so between 1 and 9
+print(random_number) # with print you are printing to the terminal, and in this case you are printing what's inside the variable random_number
+# so print will print what is inside the parenthesis
