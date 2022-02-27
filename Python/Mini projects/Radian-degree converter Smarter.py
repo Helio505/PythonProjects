ce736b8900418c266462036f345cf1acc0a2311e
Helio <95880348+HelioHBR@users.noreply.github.com>
2022-02-27 01:05:33 -0300

    Create Radian-degree converter Smarter.py

---
+++ Python/Mini projects/Radian-degree converter Smarter.py
@@ -0,0 +1,13 @@
+import math
+
+operation_choice = str(input("Radians to Degrees, or Degrees to Radians? rd/dr: "))
+
+if operation_choice == "rd":
+    radian_value = float(input("Put the radian value here: "))
+    rad_to_deg_calc = math.degrees(radian_value)
+    print(rad_to_deg_calc)
+
+if operation_choice == "dr":
+    degree_value = float(input("Put the degree value here: "))
+    deg_to_rad_calc = math.degrees(degree_value)
+    print(deg_to_rad_calc)
