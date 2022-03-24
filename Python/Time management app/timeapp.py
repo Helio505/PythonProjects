079a6daf660d541fad41a3352a5c3ac540685f7b
Helio <heliob_8@hotmail.com>
2022-03-24 11:58:21 -0300

    adding stuff

--- Python/Time management app/timeapp.py
+++ Python/Time management app/timeapp.py
@@ -6,6 +6,7 @@ import datetime
 from tkinter import *
 from tkinter import END
 from datetime import datetime
+from tkinter import ttk
 
 
 def dark_mode_toggle():
@@ -29,7 +30,6 @@ def dark_mode_toggle():
 
     root_toggle.mainloop()
 dark_mode_toggle()
-
 
 
 # darkmode:
@@ -58,6 +58,7 @@ def current_time():
     global time_now
     time_now = time.time()
     return time_now
+
 
 
 root = Tk()
@@ -140,7 +141,7 @@ def stopwatch_finish_calculate():
 
     date = datetime.now()
     str_date = str(date)
-    with open("23a03.txt", "a") as file:
+    with open("tasks saved file.txt", "a") as file:
         file.write(str(task_name) + "\n")
         file.write(str_date + "\n")
         file.write(display_time + "\n"*2)
@@ -162,6 +163,44 @@ def update():
     entry_box_time_passed.delete(0, END)
     entry_box_time_passed.insert(0, (str(keep_track_round), "sec"))
 
+def show_file():
+    with open("tasks saved file.txt") as file:
+        test = file.read()
+
+    root_show_file = Tk()
+    root_show_file.geometry("350x600")
+    root_show_file.configure(bg=r_w_c)
+
+    textbox = Text(root_show_file, height=30, width=40, bg=r_w_c, fg=f_c, font=14)
+    textbox.insert(END, test) # why END?
+    textbox.grid(row=1, column=3)
+
+    scrollbar = Scrollbar(root_show_file, orient="vertical", command=textbox.yview)
+
+    scrollbar.grid(row=1, column=4, sticky='ns')
+
+    textbox["yscrollcommand"] = scrollbar.set
+
+    root_show_file.mainloop()
+
+
+    # labeltest = Label(root, width=40)
+    # # labeltest.insert(0, test)
+    # labeltest.grid(row=1, column=3)
+
+    # text = Text(labeltest)
+    # text.grid(row=1, column=4)
+
+    # spacing_widget = Label(root, width=4, bg=r_w_c)
+    # spacing_widget.grid(row=1, column=4)
+
+    # testscroll = Scrollbar(labeltest, command=labeltest)
+    # text.config(yscrollcommand=testscroll.set)
+    # testscroll.grid(row=1, column=5)
+    # labeltest.config(xscrollcommand=testscroll.set)
+
+
+
 
 # Buttons:
 button_stopwatch_start = Button(root, text=" Start ", padx=40, pady=20, bg=b_c, fg=f_c, command=lambda: stopwatch_start())
@@ -174,6 +213,7 @@ button_clear = Button(root, text=" Clear ", padx=40, pady=20, bg=b_c, fg=f_c, co
 
 button_show_time = Button(root, text="Show time", padx=28, pady=20, bg=b_c, fg=f_c, command=lambda: update())
 
+button_show_file = Button(root, text="Show file", padx=28, pady=20, bg=b_c, fg=f_c, command=lambda: show_file())
 
 # button_dark_mode = Button(root, text="Darkmode", padx=28, pady=20, bg=b_c, fg=f_c, command=lambda: dark_mode_toggle())
 
@@ -183,7 +223,7 @@ button_stopwatch_start.grid(row=2, column=0)
 button_stopwatch_finish_calculate.grid(row=3, column=0)
 button_clear.grid(row=4, column=0)
 button_show_time.grid(row=5, column=0)
-
+button_show_file.grid(row=6, column=0)
 
 # button_dark_mode.grid(row=5, column=1)
 
