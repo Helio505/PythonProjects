b9593ffd07a4e5c1db452d0544ee9101408e1632
Helio <heliob_8@hotmail.com>
2022-06-20 15:41:31 -0300

    Refactored old code.

--- Python/Mini projects/Dice roller.py
+++ Python/Mini projects/Dice roller.py
@@ -1,7 +1,8 @@
+"""
+    -- Dice roller --
+    objective: you'll run the code and python will print a random number from 1 to 9
+"""
+
 import random
-
-# Dice roller
-# objective: you'll run the code and python will print a random number from 1 to 9
-
 random_number = random.randint(1, 9)
 print(random_number)
--- Python/Mini projects/Even number detector.py
+++ Python/Mini projects/Even number detector.py
@@ -1,11 +1,11 @@
-## Even number detector
-# Input number and it will analyse if it's even or not.
-import math
+"""
+    -- Even number detector --
+    Input number and it will analyse if it's even or not.
+"""
 
 input_number = int(input("Put the number here: "))
 
-# Remainder of (input_number / 2)
-if math.remainder(input_number, 2) == 0:
-    print("Is even")
+if (input_number % 2) == 0:
+    print(f"{input_number} Is even")
 else:
-    print("Isn't even")
+    print(f"{input_number} Is odd")
